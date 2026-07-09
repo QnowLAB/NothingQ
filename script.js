6ddda92acc9f32469eb5af0e38945ed48fe32f61
QnowLAB <nzmsys@gmail.com>
2026-07-09 07:13:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Welcome to NothingQ");
+let score = 0;
+
+const circle = document.getElementById("circle");
+const points = document.getElementById("points");
+
+circle.addEventListener("click", () => {
+
+    score++;
+
+    points.textContent = score;
+
+    const x = Math.random() * (window.innerWidth - 40);
+
+    const y = Math.random() * (window.innerHeight - 40);
+
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+
+});
