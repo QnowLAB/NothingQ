2f00fed4abfdbf5a4388a365a921a48af543be6d
QnowLAB <nzmsys@gmail.com>
2026-07-11 05:22:49 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+
+    START_LIVES:3,
+
+    START_SPEED:1000,
+
+    MIN_SPEED:250,
+
+    SPEED_STEP:70,
+
+    LEVEL_POINTS:25,
+
+    MAX_PARTICLES:18
+
+};
+
+const SHOP = {
+
+    LIFE:100,
+
+    COMBO:50,
+
+    SLOW:75
+
+};
